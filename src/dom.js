c642198381d8a2a4628ba7c3025f8f240385c26f
sujan <sujanpoojary884@gmail.com>
2019-12-09 21:09:47 +0530

    webpack import module

---
+++ src/dom.js
@@ -0,0 +1,19 @@
+
+
+console.log('dom file');
+
+const body = document.querySelector('body');
+
+const styleBody = () => {
+    body.style.background = '#e45673';
+};
+
+const addTitle = (text) => {
+    const title = document.createElement('h1');
+    title.textContent = text;
+    body.appendChild(title);
+};
+
+styleBody();
+addTitle('hello world from Ninja');
+
--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
 
+import './dom';
 
-const greet = () => {
-    console.log('Hello world!');
-}
-
-greet();
-
-const jsTutorial = () => {
-    console.log('Modern javascript');
-}
-
-jsTutorial();
-
-class Student {
-    constructor() {
-        this.name = "Sujan",
-        this.age = 21,
-        this.branch = 'MCA@MIT'
-        this.id = '19097004'
-    }
-    grtStudentInfo() {
-        console.log('Name: ', this.name);
-        console.log('Branch: ', this.branch);
-        console.log('Id: ', this.id);
-    }
-}
+console.log('index file');
